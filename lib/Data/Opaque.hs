a8ed992ff8a4c218100c50f282db7587a13d3eba
Maštarija <51950685+mastarija@users.noreply.github.com>
2020-10-31 11:56:20 +0100

    played with generics a bit

--- lib/Data/Opaque.hs
+++ lib/Data/Opaque.hs
@@ -1,15 +1,28 @@
+{-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE DefaultSignatures #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TypeSynonymInstances #-}
 --
 module Data.Opaque where
 --
 import Data.Text            ( Text )
 import Data.Vector          ( Vector )
 import Data.ByteString      ( ByteString )
-import Data.Scientific      ( Scientific )
 import Data.HashMap.Strict  ( HashMap )
 --
-import GHC.Generics         ( Generic )
+import GHC.Generics         ( Generic (..) , Datatype (..) , Constructor (..) , M1 (..) , D1 , C1 , (:+:) (..) )
 --
+
+type OLabel  = Text
+
+type OBool   = Bool
+type OBinary = ByteString
+type ONumber = Double
+type OString = String
+type OVector = [ Opaque ]
+type ORecord = HashMap OLabel Opaque
 
 data Opaque
   = ONull
@@ -19,13 +32,23 @@ data Opaque
   | OString !OString
   | OVector !OVector
   | ORecord !ORecord
-  deriving ( Eq, Show, Generic )
+  deriving ( Eq , Show , Generic )
 
-type OBool   = Bool
-type OBinary = ByteString
-type ONumber = Scientific
-type OString = Text
-type OVector = Vector Opaque
-type ORecord = HashMap OLabel Opaque
+class EncodeOpaque v where
+  encodeOpaque :: v -> Opaque
+  default
+    encodeOpaque :: ( Generic v , EncodeOpaque' ( Rep v ) ) => v -> Opaque
+  encodeOpaque = encodeOpaque' . from
 
-type OLabel  = Text
+class EncodeOpaque' f where
+  encodeOpaque' :: f p -> Opaque
+
+instance EncodeOpaque' f => EncodeOpaque' ( D1 c f ) where
+  encodeOpaque' ( M1 v ) = encodeOpaque' v
+
+instance Constructor c => EncodeOpaque' ( C1 c f ) where
+  encodeOpaque' m@( M1 _ ) = OString $ conName m
+
+instance ( EncodeOpaque' f , EncodeOpaque' g ) => EncodeOpaque' ( f :+: g ) where
+  encodeOpaque' ( L1 v ) = encodeOpaque' v
+  encodeOpaque' ( R1 v ) = encodeOpaque' v
